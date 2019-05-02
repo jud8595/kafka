45160ba07d2fef4af15a928012e9b4831c00b7f1
jdeshayes <jdeshayes.ext@galerieslafayette.com>
2019-05-02 17:45:48 +0200

    basic example

--- src/main/java/com/gl/tuto/kafka/KafkaApplication.java
+++ src/main/java/com/gl/tuto/kafka/KafkaApplication.java
@@ -1,13 +1,24 @@
 package com.gl.tuto.kafka;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KafkaApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(KafkaApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaApplication.class, args);
+	}
+
+	@Bean
+	public NewTopic adviceTopic() {
+		return new NewTopic("users", 3, (short) 1);
 	}
 
 }
